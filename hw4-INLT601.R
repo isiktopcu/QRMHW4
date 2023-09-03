f0eb7f523e6f75c1e34e4c539b26f0489111db22
Işık Topçu <91604042+isiktopcu@users.noreply.github.com>
2023-09-03 13:33:19 +0300

    Add files via upload

--- hw4-INLT601.R
+++ hw4-INLT601.R
@@ -112,19 +112,19 @@ df$age_of_current_regime[df$age_of_current_regime>= 999]<-NA
 
 #this can go on and on and on.... to accommodate all the chosen variables but given our time constraints, we chose the following 
 ----------------------------------------------------------------
-#Logistic Regression (we have a binary outcome)
-
-mod=glm(turnout_main_election ~ inflation*ideology + 
-          education + 
-          polity+
-          age + 
-          voting_procedure + 
-          gdp_per_cap + 
-          religiosity + 
-          unemployment*ideology + 
-          political_information + 
-          age_of_current_regime,
-        family = binomial, data=df,na.action = na.omit)
+  #Logistic Regression (we have a binary outcome)
+  
+  mod=glm(turnout_main_election ~ inflation*ideology + 
+            education + 
+            polity+
+            age + 
+            voting_procedure + 
+            gdp_per_cap + 
+            religiosity + 
+            unemployment*ideology + 
+            political_information + 
+            age_of_current_regime,
+          family = binomial, data=df,na.action = na.omit)
 summary(mod)
 #the intercept is significant given that even without the effects of independent variables, 
 #its highly possible there are other factors which explain vote turnout
@@ -144,9 +144,9 @@ hist(deviance_resid, breaks=30, main="Histogram of Deviance Residuals")
 plot(predict(mod, type="response"), deviance_resid, xlab="Fitted values", ylab="Deviance residuals")
 
 ---------------------------------------------------
-#Fixed Effects 
-
-install.packages("plm")
+  #Fixed Effects 
+  
+  install.packages("plm")
 library(plm)
 fixed <- plm(turnout_main_election ~ unemployment + ideology + education + 
                polity + age + voting_procedure + gdp_per_cap + inflation + 
@@ -165,30 +165,31 @@ fixed <- glm(turnout_main_election ~ unemployment * ideology + education +
              na.action = na.omit)
 summary(fixed)
 
-#for each unit of analysis (individuals, states, or years, depending on our data), 
-#we would need to account for time-invariant unobserved characteristics in order to focus on the estimates of the other variables (such as unemployment, education, etc.). 
-#if we believe that there are unobserved factors that affect voter turnout and 
-#are related to our independent variables, this will provide us objective estimates of the key variables.
-#age, education, politics, and unemployment are just a few of the variables that are statistically significant.
+#in order to concentrate on the estimates of the other variables (such as unemployment, education, etc.), 
+#we would need to adjust for time-invariant unobserved features for each unit of analysis (individuals, states, or years, depending on our data). 
+#this will provide us unbiased estimates of the important variables, 
+#which is especially helpful if you think that there are unobserved factors that influence voter turnout 
+#and are connected with our independent variables.
+#Numerous factors, including age, education, politics, and unemployment, have statistical significance.
 ---------------------------------------------------------------------------------
-
-#Two way fixed effect ( unit + time) 
-
-fixed2=glm(turnout_main_election ~ unemployment* ideology + education + polity+
-            age + voting_procedure + gdp_per_cap + inflation + political_information + age_of_current_regime + 
-            religiosity+as.factor(country)+as.factor(year_of_election),
-          family = binomial, data=df,na.action = na.omit)
+  
+  #Two way fixed effect ( unit + time) 
+  
+  fixed2=glm(turnout_main_election ~ unemployment* ideology + education + polity+
+               age + voting_procedure + gdp_per_cap + inflation + political_information + age_of_current_regime + 
+               religiosity+as.factor(country)+as.factor(year_of_election),
+             family = binomial, data=df,na.action = na.omit)
 summary(fixed2)
 
 
-#most predictors are significant for example, 
+#most predictors are significant and suggest practical implications—for example, 
 #a rise in unemployment seems to deter voter turnout. 
 --------------------------------------------------------------------------------
-
-#Random Effects Model 
-
-
-library(lme4)
+  
+  #Random Effects Model 
+  
+  
+  library(lme4)
 random <- glmer(turnout_main_election ~ unemployment*ideology + education + polity +
                   age + voting_procedure + gdp_per_cap + inflation + 
                   political_information + age_of_current_regime + religiosity + (1|country) + (1|year_of_election), 
@@ -196,17 +197,17 @@ random <- glmer(turnout_main_election ~ unemployment*ideology + education + poli
 summary(random)
 
 ---------------------------------------------------------------------------------
-#Mixed Effects Model 
-
-mixed <- glmer(turnout_main_election ~ unemployment*ideology + education + polity +
-                 age + voting_procedure + gdp_per_cap + inflation + 
-                 political_information + age_of_current_regime + religiosity + (1|country) + (1|year_of_election), 
-               family = binomial, data=df, na.action = na.omit)
+  #Mixed Effects Model 
+  
+  mixed <- glmer(turnout_main_election ~ unemployment*ideology + education + polity +
+                   age + voting_procedure + gdp_per_cap + inflation + 
+                   political_information + age_of_current_regime + religiosity + (1|country) + (1|year_of_election), 
+                 family = binomial, data=df, na.action = na.omit)
 summary(mixed)
 -------------------------------------------------------------------------------
-
-#Standart errors  
-library(sandwich)
+  
+  #Standart errors  
+  library(sandwich)
 install.packages("multiwayvcov")
 library(multiwayvcov)
 library(lmtest)
@@ -214,9 +215,9 @@ vcovCL1 <- cluster.vcov(mod, ~as.factor(df$country) + as.factor(df$year_of_elect
 coeftest(mod, vcovCL1)
 
 ---------------------------------------------------------------------------------
-#what's next? 
-
-# 1 asssess the usefulness of predictors over a null model by benchmarking the #null model.
-# include dummies for categories like gender and socioeconomic status when using categorical variables.
-# model choice: Use a random effects model that is supported by a Hausman test.
-# Test models using inflation and GDP growth as extra explanatory variables to identify alternative economic indicators.
+  #what's next? 
+  
+  #null model benchmarking: Assess the added value of predictors over a null model.
+  #categorical variables: Include dummies for categories like gender and socio-economic status.
+  #model selection: Employ a random effects model, justified via a Hausman test.
+  #alternative economic indicators: Test models using inflation and GDP growth as additional explanatory variables.
